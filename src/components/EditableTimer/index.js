2aa33d9b518131d842c9bb37ee98b395525c52d2
Melvin Gaye <melvind.gaye@gmail.com>
2020-10-05 22:45:57 -0400

    changed duplicate title prop to project prop

---
+++ src/components/EditableTimer/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Timer from '../Timer/index'
+import TimerForm from '../TimerForm/index'
+
+class EditableTimer extends React.Component{
+    render(){
+        if(this.props.editFormOpen){
+            return(<TimerForm
+                    title={this.props.title}
+                    project={this.props.project}
+                    />
+                    );
+        } else {
+            return (
+                <Timer
+                 title={this.props.title}
+                 project={this.props.project}
+                 elapsed={this.props.elapsed}
+                 runningSince={this.props.runningSince}
+                 />
+            );
+        }
+    }
+}
+
+export default EditableTimer;
---
+++ src/components/EditableTimerList/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import EditableTimer from '../EditableTimer/index'
+
+class EditableTimerList extends React.Component {
+    render(){
+        return(
+            <div id="timers">
+                <EditableTimer
+                 title='Learn React'
+                 project='Web Domination'
+                 elapsed='8986300'
+                 runningSince={null}
+                 editFormOpen={false}
+                 />
+                 <EditableTimer
+                 title='Learn extreme walking'
+                 project='Household Domination'
+                 elapsed='3896300'
+                 runningSince={null}
+                 editFormOpen={true}
+                 />
+            </div>
+        )
+    }
+}
+
+export default EditableTimerList;
